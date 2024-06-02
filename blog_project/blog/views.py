0625360854d1f57f888b1792537d1b85c4812e61
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-06-02 14:17:38 +0500

    done

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,22 +1,29 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from .models import Blog
 
 # Create your views here.
 
-def blogListView(request):
-    blogs = Blog.objects.all()
+# def blogListView(request):
+#     blogs = Blog.objects.all()
+#
+#     context = {
+#         "blogs":blogs,
+#     }
+#
+#     return render(request, 'home.html', context=context)
+#
+# def blogDetailView(request, id):
+#     blog = get_object_or_404(Blog, id=id)
+#     context = {
+#         "blog":blog,
+#     }
+#
+#     return render(request, "blog_detail.html", context=context)
 
-    context = {
-        "blogs":blogs,
-    }
 
-    return render(request, 'home.html', context=context)
-
-def blogDetailView(request, id):
-    blog = get_object_or_404(Blog, id=id)
-    context = {
-        "blog":blog,
-    }
-
-    return render(request, "blog_detail.html", context=context)
+class BlogListView(ListView):
+    model = Blog
+    template_name = "home.html"
