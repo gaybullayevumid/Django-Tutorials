3e9ec42d9eb5d7893822b147fceefa7270678f74
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-06-11 11:46:39 +0500

    done

--- news_project/news_app/urls.py
+++ news_project/news_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import news_list
+from .views import news_list, news_detail, homePageView
 
 urlpatterns = [
-    path('news/', news_list, name='all_news_list')
+    path('', homePageView, name='home_page'),
+    path('news/', news_list, name='all_news_list'),
+    path('news/<int:id>/', news_detail, name='news_detail_page'),
 ]
--- news_project/news_project/settings.py
+++ news_project/news_project/settings.py
@@ -117,8 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
