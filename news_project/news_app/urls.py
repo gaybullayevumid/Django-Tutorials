eef20ca3d984bb50813d4211042b70ce29cdb5a1
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-06-11 15:43:17 +0500

    done

--- news_project/news_app/urls.py
+++ news_project/news_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView
+from .views import news_list, news_detail, homePageView, contactPageView
 
 urlpatterns = [
     path('', homePageView, name='home_page'),
     path('news/', news_list, name='all_news_list'),
     path('news/<int:id>/', news_detail, name='news_detail_page'),
+    path('contact-us/', contactPageView, name='contact_page')
 ]
