387ab015ddeae61eac7eecf316801fa9d08f5e8d
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-06-19 10:50:39 +0500

    done

---
+++ news_project/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .form import LoginForm
+
+# Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            print(data)
+            user = authenticate(request, username=data['username'], password=data['password'])
+            print(user)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Muvaffaqiyatli login amalga oshirildi')
+                else:
+                    return HttpResponse('Sizning profilingiz faol holatda emas')
+            else:
+                return HttpResponse('Login yoki parolda xatolik bor!')
+    else:
+        form = LoginForm()
+        
+    return render(request, 'account/login.html', {'form':form})
