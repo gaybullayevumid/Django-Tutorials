980144b3912af1f1261fe049431642b66822bfb2
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-06-19 08:34:39 +0500

    done

--- news_project/news_app/views.py
+++ news_project/news_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import News, Category
 from .forms import ContactForm
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -125,3 +126,13 @@ class SportNewsView(ListView):
     def get_queryset(self):
        news = self.model.published.all().filter(category__name="Sport")
        return news
+    
+class NewsUpdateView(UpdateView):
+    model = News
+    fields = ('title', 'body', 'image', 'category', 'status',)
+    template_name = 'crud/news_edit.html'
+
+class NewsDeleteView(DeleteView):
+    model = News
+    template_name = 'crud/news_delete.html'
+    success_url = reverse_lazy('home_page')
