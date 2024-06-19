c19b6ef5c29554df07e7af7c255214106812550a
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-06-19 09:05:11 +0500

    done

--- news_project/news_app/admin.py
+++ news_project/news_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import News, Category, Contact
+
 # Register your models here.
 
 @admin.register(News)
--- news_project/news_app/urls.py
+++ news_project/news_app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('news/', news_list, name='all_news_list'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
     path('news/<slug:news>/', news_detail, name='news_detail_page'),
     path('news/<slug>/edit/', NewsUpdateView.as_view(), name="news_update"),
     path('news/<slug>/delete/', NewsDeleteView.as_view(), name="news_delete"),
